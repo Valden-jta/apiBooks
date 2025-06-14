a5bd87068b93c96cd055fa96781214a4a17a23ca
Valden-jta <javierdlt45@gmail.com>
2025-06-14 16:41:41 +0200

    añadida comprobacion de mail antes de registro

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -21,9 +21,17 @@ DELETE      =   DELETE
 const postUser = async (req, res) => {
   try {
     // Validación
-    if (!req.body.name || !req.body.last_name || !req.body.email || !req.body.photo || !req.body.password) {
-        return res.status(422).json({ error: "Todos los campos son obligatorios" });
-    } 
+    if (
+      !req.body.name ||
+      !req.body.last_name ||
+      !req.body.email ||
+      !req.body.photo ||
+      !req.body.password
+    ) {
+      return res
+        .status(422)
+        .json({ error: "Todos los campos son obligatorios" });
+    }
 
     newUser = new User(
       req.body.id_user,
@@ -33,21 +41,42 @@ const postUser = async (req, res) => {
       req.body.photo,
       req.body.password
     );
+
+    // Comprobar si el mail existe en la base de datos
+    let sql = `SELECT email FROM user WHERE email = ?`;
+    let param = [req.body.email];
+    let [emailCheck] = await pool.query(sql, param);
+
+    if (emailCheck.length) {
+      return res.status(409).json({
+        error: true,
+        code: 409,
+        message: "El email ya está registrado",
+      });
+    }
     newUser.password = await hashPassword(newUser.name, newUser.password);
     console.log("Usuario a insertar: ", newUser);
-    
+
     // Insertar user en la DB
-    let sql = `INSERT INTO user (name, last_name, email, photo, password)
+    sql = `INSERT INTO user (name, last_name, email, photo, password)
     VALUES (?,?,?,?,?)`;
-    let param = [newUser.name, newUser.last_name, newUser.email, newUser.photo, newUser.password];
+    param = [
+      newUser.name,
+      newUser.last_name,
+      newUser.email,
+      newUser.photo,
+      newUser.password,
+    ];
 
     let [result] = await pool.query(sql, param);
     console.log("Reusltado de la consulta: ", result);
 
     if (result.insertId)
-        res.status(201).json({ message: "Usuario creado correctamente. Id de usuario: " + result.insertId });
-    else 
-        res.status(500).json({ message: "No se pudo crear el usuario" });
+      res.status(201).json({
+        message:
+          "Usuario creado correctamente. Id de usuario: " + result.insertId,
+      });
+    else res.status(500).json({ message: "No se pudo crear el usuario" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
