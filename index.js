55f8a32933496837813e398b99501569f17e3b6e
Valden-jta <javierdlt45@gmail.com>
2025-06-29 22:07:15 +0200

    deploy settings for vercel

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // *---------------------- template ---------------------- *\\
 
-const app = require("./app");
+const app = require("./src/app");
 
 app.listen(app.get("port"), () => {
   console.log(`Server listening on port ${app.get('port')}`);
